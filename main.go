5c0e54886ea67cf9fa559f8244f6739ec502a77f
BRUGUSOFT <kiran.paturi@brugu.io>
2023-09-14 12:48:21 +0530

    added chages

--- main.go
+++ main.go
@@ -9,6 +9,39 @@ func init(){
 func init(){
 	fmt.Printf("I am init 2 \n")
 }
+
+func forRange(){
+	numbers := []int{1,34,5,7,6}
+     
+	for index,item := range numbers{
+		 fmt.Println("item - ",item)
+		 fmt.Println("index = ",index)
+	}
+
+}
+
+func slices (){
+	 marks := [8]int{40,50,60,69,8,90,10,2}
+	 s := marks[1:4]
+	 fmt.Println(s)
+	 fmt.Println(len(s))
+	 fmt.Println(cap(s))
+	//  for _,item := range marks{
+	// 	fmt.Println(item)
+	//  }
+	//  for _,item1 := range s{
+	// 	fmt.Println(item1)
+	//  }
+}
+
+
 func main() {
-	fmt.Println("Welcome to go ")
+	item, price := "Mobile", 2000
+	fmt.Println("Welcome to go: ",item,price)
+	if price>=2000 {
+		//do this
+		fmt.Println("price is greater than 2000\n")
+	}
+	//forRange()
+	slices()
 }
